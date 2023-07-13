c169b0bb5493a8a50f21883ef844b7db9f1093ac
dxd8201 <ddronet17@gmail.com>
2023-07-12 21:18:22 -0500

    set up css assest manager

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import './style.css';
+
+
 function component() {
-    // const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
   console.log("Hello world")
-    // return element;
+   
   }
   
   component();
